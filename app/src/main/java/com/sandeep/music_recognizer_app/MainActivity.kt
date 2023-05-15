9c59e718e989cb7b72ea23fb6fbe2509f64d9f58
mssandeepkamath <msandeepcip@gmail.com>
2023-05-15 18:26:37 +0530

    implemented mediaProjectManager for listening

--- app/src/main/java/com/sandeep/music_recognizer_app/MainActivity.kt
+++ app/src/main/java/com/sandeep/music_recognizer_app/MainActivity.kt
@@ -2,42 +2,93 @@ package com.sandeep.music_recognizer_app
 
 
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.projection.MediaProjectionManager
+import android.nfc.NfcAdapter.EXTRA_DATA
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    private  var data: Intent? = null
+    private lateinit var mediaProjectionManager: MediaProjectionManager
+    private var isServiceStarted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkOverlayPermission()
-        startService()
+
+        if(checkOverlayPermission())
+        {
+            startCapturing()
+        }
+        else
+        {
+            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            startActivity(myIntent)
+            Toast.makeText(this,"Reopen the app, once permitted",Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+    private fun startCapturing() {
+        if (!isRecordAudioPermissionGranted()) {
+            requestRecordAudioPermission()
+        } else {
+            startMediaProjectionRequest()
+        }
     }
 
-    // method for starting the service
-    private fun startService() {
-        // check if the user has already granted
-        // the Draw over other apps permission
-        if (Settings.canDrawOverlays(this)) {
-            // start the service based on the android version
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(Intent(this, ForegroundService::class.java))
-            } else {
-                startService(Intent(this, ForegroundService::class.java))
+    private fun startMediaProjectionRequest() {
+        mediaProjectionManager =
+            applicationContext.getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startActivityForResult(
+            mediaProjectionManager.createScreenCaptureIntent(),
+            MEDIA_PROJECTION_REQUEST_CODE
+        )
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        isServiceStarted = true
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            MEDIA_PROJECTION_REQUEST_CODE -> {
+                if (resultCode == RESULT_OK) {
+                    startOverlayService(data!!)
+                } else {
+                    Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
 
-    private fun checkOverlayPermission() {
-        if (!Settings.canDrawOverlays(this)) {
-            // send user to the device settings
-            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-            startActivity(myIntent)
-        }
+    // method for starting the service
+    private  fun startOverlayService(data:Intent) {
+            // start the service based on the android version
+
+            val serviceintent = Intent(this,ForegroundService::class.java).apply {
+                action = ForegroundService.ACTION_START
+                putExtra(ForegroundService.EXTRA_RESULT_DATA, data)
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(serviceintent)
+            } else {
+                startService(serviceintent)
+            }
+    }
+
+    private fun checkOverlayPermission() : Boolean{
+        return Settings.canDrawOverlays(this)
     }
 
     override fun onDestroy() {
@@ -46,9 +97,47 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    override fun onResume() {
-        super.onResume()
-        startService()
+    companion object {
+        private const val RECORD_AUDIO_PERMISSION_REQUEST_CODE = 42
+        private const val MEDIA_PROJECTION_REQUEST_CODE = 13
+    }
+
+    private fun requestRecordAudioPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.RECORD_AUDIO),
+            RECORD_AUDIO_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun isRecordAudioPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.RECORD_AUDIO
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == RECORD_AUDIO_PERMISSION_REQUEST_CODE) {
+            if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(
+                    this,
+                    "Permissions to capture audio granted. Click the button once again.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this, "Permissions to capture audio denied.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
 
